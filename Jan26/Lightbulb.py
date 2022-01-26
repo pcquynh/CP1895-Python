f16372b4b26ca71bb01530ef7eff0724b59f218a
pcquynh <pcquynh@mun.ca>
2022-01-26 14:37:02 -0330

    add brightness attribute

--- Jan26/Lightbulb.py
+++ Jan26/Lightbulb.py
@@ -1,9 +1,10 @@
 class Lightbub:
-    def __init__(self, wattage: int, is_led: bool, brand_name: str, is_on: bool=False):
+    def __init__(self, wattage: int, is_led: bool, brand_name: str, is_on: bool, brightness: int):
         self.wattage = wattage
         self.is_led = is_led
         self.brand_name = brand_name
         self.is_on = is_on
+        self.brightness = brightness
 
     def turn_on(self):
         self.is_on = True
@@ -14,11 +15,11 @@ class Lightbub:
     def to_string(self):
         print(self.wattage, self.brand_name)
 
-
     def set_brightness(self, level):
-        print(f"Brightness level is {level}")
+        self.brightness = level
+        print(f"Brightness level is {self.brightness}")
 
 
-l1 = Lightbub(100, False, "Phillip" )
+l1 =Lightbub(100, False, "Phillip", False, 2)
 l1.to_string()
-l1.set_brightness(5)
+l1.set_brightness(6)
